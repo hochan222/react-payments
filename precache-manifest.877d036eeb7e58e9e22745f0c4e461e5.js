a7c70a980d8988e87cb9aa91b37e3d0b9158aa1b
hochan Lee <hochan049@gmail.com>
2021-06-19 21:36:16 +0900

    Updates

---
+++ precache-manifest.877d036eeb7e58e9e22745f0c4e461e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252b56587041722debc6d2e3a046d61d",
+    "url": "/react-payments/index.html"
+  },
+  {
+    "revision": "86fc4d9cdfa82a93212c",
+    "url": "/react-payments/static/css/main.9fd0b1bd.chunk.css"
+  },
+  {
+    "revision": "69b89d39c06806e72ede",
+    "url": "/react-payments/static/js/2.df4cd04b.chunk.js"
+  },
+  {
+    "revision": "86fc4d9cdfa82a93212c",
+    "url": "/react-payments/static/js/main.492ef749.chunk.js"
+  },
+  {
+    "revision": "e92d155d5799229562b9",
+    "url": "/react-payments/static/js/runtime~main.df008c84.js"
+  }
+]);
